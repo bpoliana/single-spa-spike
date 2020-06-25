c4f3f1511692221d72e5f9212a07c3eb62ca6523
bpoliana <bpolianajaber@gmail.com>
2020-06-25 13:43:40 -0300

    Basic single-spa application parcel configs

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const webpackMerge = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
+
+module.exports = (webpackConfigEnv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "tw",
+    projectName: "single-spa-poc",
+    webpackConfigEnv,
+  });
+
+  return webpackMerge.smart(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+  });
+};
