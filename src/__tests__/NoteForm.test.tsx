36e5496d04cbf24f149fd02ffda35c21243cf643
bpoliana <bpolianajaber@gmail.com>
2020-06-25 17:25:45 -0300

    Add NoteForm component and its tests

---
+++ src/__tests__/NoteForm.test.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import NoteForm, { Props } from "../components/NoteForm";
+
+function renderNoteForm(props: Partial<Props> = {}) {
+  const defaultProps: Props = {
+    onTitleChange() {
+      return;
+    },
+    onContentChange() {
+      return;
+    },
+    onImportanceChange() {
+      return;
+    },
+    onSubmit() {
+      return;
+    },
+    isImportant: true,
+  };
+  return render(<NoteForm {...defaultProps} {...props} />);
+}
+
+describe("NoteForm Component", () => {
+  test("should allow entering a title", async () => {
+    const onTitleChange = jest.fn();
+    const { findByTestId, debug } = renderNoteForm({ onTitleChange });
+    const title = await findByTestId("title");
+
+    fireEvent.change(title, { target: { value: "test" } });
+
+    expect(onTitleChange).toHaveBeenCalledWith("test");
+  });
+
+  test("should allow setting importance to true", async () => {
+    const onImportanceChange = jest.fn();
+    const { findByTestId } = renderNoteForm({
+      onImportanceChange,
+      isImportant: false,
+    });
+
+    const isImportant = await findByTestId("importance");
+    fireEvent.click(isImportant);
+
+    expect(onImportanceChange).toHaveBeenCalledWith(true);
+
+    fireEvent.click(isImportant);
+
+    expect(onImportanceChange).toHaveBeenCalledWith(false);
+  });
+
+  test("should submit the form with title, content, and importance", async () => {
+    const onSubmit = jest.fn();
+    const { findByTestId } = renderNoteForm({
+      onSubmit,
+      isImportant: false,
+    });
+
+    const title = await findByTestId("title");
+    const content = await findByTestId("content");
+    const importance = await findByTestId("importance");
+    const submit = await findByTestId("submit");
+
+    fireEvent.change(title, { target: { value: "test" } });
+    fireEvent.change(content, { target: { value: "content" } });
+    fireEvent.click(importance);
+    fireEvent.click(submit);
+
+    expect(onSubmit).toHaveBeenCalledWith("test", "content");
+  });
+});
---
+++ src/components/NoteForm.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+interface Author {
+  firstName: string;
+  lastName: string;
+}
+
+export interface Props {
+  isImportant: boolean;
+  onTitleChange: (title: string) => void;
+  onContentChange: (content: string) => void;
+  onImportanceChange: (isImportant: boolean) => void;
+  onSubmit: (title: string, content: string) => void;
+}
+
+const NoteForm = (props: Props) => {
+  const [title, setTitle] = React.useState("");
+  const [content, setContent] = React.useState("");
+  const [isImportant, setImportance] = React.useState(props.isImportant);
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setTitle(value);
+    props.onTitleChange(value);
+  };
+
+  const handleContentChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setContent(value);
+    props.onContentChange(value);
+  };
+
+  const handleImportanceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { checked } = e.target;
+    setImportance(checked);
+    props.onImportanceChange(checked);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    props.onSubmit(title, content);
+  };
+
+  return (
+    <form data-testid="note-form" onSubmit={handleSubmit}>
+      <label htmlFor="title">Title:</label>
+      <input
+        data-testid="title"
+        type="text"
+        name="title"
+        value={title}
+        onChange={handleTitleChange}
+      />
+
+      <label htmlFor="content">Content:</label>
+      <input
+        data-testid="content"
+        type="text"
+        name="content"
+        value={content}
+        onChange={handleContentChange}
+      />
+
+      <label>
+        <input
+          data-testid="importance"
+          type="checkbox"
+          name="isImportant"
+          checked={isImportant}
+          onChange={handleImportanceChange}
+        />
+      </label>
+
+      <button data-testid="submit" type={"submit"} datatype={"submit"}>
+        {" "}
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default NoteForm;
